b5ae77dc81af5383300b2808716c18fc1a6ebbdb
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-07-08 20:48:44 -0400

    Export functions

--- convert.go
+++ convert.go
@@ -2,7 +2,7 @@ package convert
 
 import "syscall/js"
 
-func convertToGoType(dataValue js.Value) interface{} {
+func ToGoType(dataValue js.Value) interface{} {
 	dataType := dataValue.Type()
 	var data interface{}
 	switch dataType {
@@ -11,7 +11,7 @@ func convertToGoType(dataValue js.Value) interface{} {
 	case js.TypeNumber:
 		data = dataValue.Int()
 	case js.TypeObject:
-		data = convertJSObjectToGoType(dataValue)
+		data = JSObjectToGoType(dataValue)
 	case js.TypeString:
 		data = dataValue.String()
 	case js.TypeNull:
@@ -24,56 +24,56 @@ func convertToGoType(dataValue js.Value) interface{} {
 	return data
 }
 
-func convertJSObjectToGoType(value js.Value) interface{} {
+func JSObjectToGoType(value js.Value) interface{} {
 	if value.InstanceOf(js.Global().Get("Uint8Array")) {
-		return convertToBytes(value)
+		return ToBytes(value)
 	} else if js.Global().Get("Array").Get("isArray").Invoke(value).Bool() {
-		return convertToArray(value)
+		return ToArray(value)
 	} else if value.InstanceOf(js.Global().Get("Map")) {
-		return convertJSMapToGoMap(value)
+		return JSMapToGoMap(value)
 	}
-	return convertJSObjectToGoMap(value)
+	return JSObjectToGoMap(value)
 }
 
-func convertToBytes(value js.Value) []byte {
+func ToBytes(value js.Value) []byte {
 	length := value.Length()
 	bytes := make([]byte, length)
 	js.CopyBytesToGo(bytes, value)
 	return bytes
 }
 
-func convertToArray(value js.Value) []interface{} {
+func ToArray(value js.Value) []interface{} {
 	length := value.Length()
 	array := make([]interface{}, length)
 	for i := 0; i < length; i++ {
-		array[i] = convertToGoType(value.Index(i))
+		array[i] = ToGoType(value.Index(i))
 	}
 	return array
 }
 
-func convertJSObjectToGoMap(value js.Value) map[string]interface{} {
+func JSObjectToGoMap(value js.Value) map[string]interface{} {
 	object := make(map[string]interface{})
 	keys := js.Global().Get("Object").Call("keys", value)
 	length := keys.Length()
 	for i := 0; i < length; i++ {
 		key := keys.Index(i).String()
-		object[key] = convertToGoType(value.Get(key))
+		object[key] = ToGoType(value.Get(key))
 	}
 	return object
 }
 
-func convertJSMapToGoMap(jsMap js.Value) map[interface{}]interface{} {
+func JSMapToGoMap(jsMap js.Value) map[interface{}]interface{} {
 	object := make(map[interface{}]interface{})
 	keys := js.Global().Get("Array").Call("from", jsMap.Call("keys"))
 	length := keys.Length()
 	for i := 0; i < length; i++ {
-		key := convertToGoType(keys.Index(i))
-		object[key] = convertToGoType(jsMap.Call("get", key))
+		key := ToGoType(keys.Index(i))
+		object[key] = ToGoType(jsMap.Call("get", key))
 	}
 	return object
 }
 
-func convertGoMapToJSObject(m map[string]interface{}) js.Value {
+func GoMapToJSObject(m map[string]interface{}) js.Value {
 	obj := js.Global().Get("Object").New(nil)
 	for k, v := range m {
 		obj.Set(k, v)
